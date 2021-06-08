4d8b938a3a96e03d6f70c1ffec712f2c8b62c5b8
paniztgp2002 <paniztgp2002@gmail.com>
2021-06-08 16:29:08 +0430

    add shopkeeper

---
+++ shopkeeper.py
@@ -0,0 +1,14 @@
+class Shopkeeper:
+    def __init__(self, user, password, product, location, score, order, wallet, sell, credit, rate, profit, code):
+        self.__user = user
+        self.__password = password
+        self.__product = product
+        self.__location = location
+        self.__score = score
+        self.__order = order
+        self.__wallet = wallet
+        self.__sell = sell
+        self.__credit = credit
+        self.__rate = rate
+        self.__profit = profit
+        self.__code = code
