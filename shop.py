fb5f831c54d8fd68a88f62378215f72f5b7ce3de
farimahmh <farimahmh>
2021-06-09 17:46:50 +0430

    update shop

--- shop.py
+++ shop.py
@@ -23,3 +23,19 @@ class shop:
     @password.setter
     def password(self, new_pass):
         self.__password = new_pass
+
+    @property
+    def location(self):
+        return self.__location
+
+    @location.setter
+    def location(self, loc):
+        self.__location = loc
+
+    @property
+    def profit(self):
+        return self.__profit
+
+    @profit.setter
+    def profit(self, new_profit):
+        self.__profit = new_profit
