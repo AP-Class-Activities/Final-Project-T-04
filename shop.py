887c745c79fd05a0f1b61034cedeccc178b16e04
paniztgp2002 <paniztgp2002@gmail.com>
2021-06-09 13:37:21 +0430

    Update shopkeeper.py

---
+++ shop.py
@@ -0,0 +1,9 @@
+class shop:
+    def __init__(self, user, password, location,profit,sold_items,shopkeepers_list,customers_list):
+        self.__user = user
+        self.__password = password
+        self.__location = location
+        self.__profit = profit
+        self.__sold_items = sold_items
+        self.__shopkeepers_list = shopkeepers_list
+        self.__customers_list = customers_list
