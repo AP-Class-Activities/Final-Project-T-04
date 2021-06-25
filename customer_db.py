6aa0f5f0b4eb27285a7e5d612c34272f7edb2f26
Hamed Teymouri <html113343@gmail.com>
2021-06-25 18:28:00 +0430

    2

--- customer_db.py
+++ customer_db.py
@@ -3,3 +3,6 @@ import json
 class DataBase:
     def __init__(self):
         self.name = "C:\\DataBase\\customer.json"
+    def read_list(self):
+        file = open(self.name,"r")
+        return json.load(file)
