2c042bea99b724e628406b749978d8f6e3f042c0
paniztgp2002 <paniztgp2002@gmail.com>
2021-06-26 12:15:35 +0430

    Update signup.py

--- signup.py
+++ signup.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_MainWindowsignup(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 800)
