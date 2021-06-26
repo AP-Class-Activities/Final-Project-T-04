c9025759f77bf5c43b2b4cc501cb2ebc58144831
paniztgp2002 <paniztgp2002@gmail.com>
2021-06-26 12:14:36 +0430

    Update customerpyqt.py

--- customerpyqt.py
+++ customerpyqt.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_MainWindowcustomerpyqt(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 800)
@@ -136,11 +136,3 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "back"))
 
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
