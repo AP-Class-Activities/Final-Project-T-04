c4748a88f274e2af7bdedc1177dfd343d1965d9c
Hamed Teymouri <html113343@gmail.com>
2021-06-28 10:21:35 +0430

    finishing customer

--- shop.py
+++ shop.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_MainWindowshop()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
