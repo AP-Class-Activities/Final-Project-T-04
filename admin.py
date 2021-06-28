568b00e5bb2284fed32e2213882cc611c3b4ed76
Hamed Teymouri <html113343@gmail.com>
2021-06-28 12:24:29 +0430

    g

--- admin.py
+++ admin.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_MainWindowadmin()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
