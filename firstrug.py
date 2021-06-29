7da477c493e61eb17be2cc5f4bc597e17bfc7c18
farimahmh <farimahmahabadii1380@gmail.com>
2021-06-29 18:01:04 +0430

    Update firstrug.py

--- firstrug.py
+++ firstrug.py
@@ -197,7 +197,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_10.setText(_translate("MainWindow", "Iranian Carpet"))
+        self.label_10.setText(_translate("MainWindow", "Rug"))
         self.label_2.setText(_translate("MainWindow", "T"))
         self.label_3.setText(_translate("MainWindow", "only"))
         self.pushButton.setText(_translate("MainWindow", "Order now"))
